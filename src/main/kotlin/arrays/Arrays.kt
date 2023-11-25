7e03f5331092ddd283c6b21e78faf0dd88f56e36
SSureshC <sureshchandak1@gmail.com>
2023-11-25 12:19:18 +0530

    Arrays

---
+++ src/main/kotlin/arrays/Arrays.kt
@@ -0,0 +1,23 @@
+package arrays
+
+/*
+*  - Object that stores multiple values of same type
+*  - Fixed size
+* */
+fun main(args: Array<String>) {
+    val arr1 = arrayOf("One", "Two", "Three")
+    val arr2 = arrayOf<Int>(1, 2, 3)
+
+    for ((index, value) in arr1.withIndex()) {
+        println("$index - $value")
+    }
+
+    println(arr1[0])
+
+    arr1[0] = "Hello"
+    println(arr1[0])
+
+    println("Size - ${ arr1.size }")
+
+    println(arr1[3]) // ArrayIndexOutOfBoundsException (IndexOutOfBoundsException)
+}
