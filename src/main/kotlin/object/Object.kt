bf292879a5a45d9bd95a332a11e4f14cd5d48d60
SSureshC <sureshchandak1@gmail.com>
2023-11-27 18:48:49 +0530

    Kotlin object keyword

---
+++ src/main/kotlin/object/Object.kt
@@ -0,0 +1,51 @@
+package `object`
+
+/*
+*  - Class and its Single Object is created at once.
+*  - No constructors are allowed (init block is allowed)
+*  - You can inherit class/interface
+*  - Singleton Pattern
+* */
+fun main() {
+    LikeObject.incrementTwitterLikes()
+    LikeObject.incrementTwitterLikes()
+    LikeObject.incrementTwitterLikes()
+    LikeObject.incrementFBLikes()
+    LikeObject.incrementFBLikes()
+    LikeObject.display()
+
+    // Object expression (Creating anonymous objects)
+    val obj = object : Cloneable {
+        override fun clone() {
+            println("I am clone")
+        }
+    }
+    obj.clone()
+
+    val person = object : Person("Kotlin") {
+        override fun fullName() {
+            super.fullName()
+            println("Anonymous - $name")
+        }
+    }
+    person.fullName()
+
+}
+
+object LikeObject {
+    private var twitterLikes = 0
+    private var fbLikes = 0
+
+    fun incrementTwitterLikes() = ++twitterLikes
+    fun incrementFBLikes() = ++fbLikes
+    fun display() = println("Twitter Likes = $twitterLikes, Facebook Likes = $fbLikes")
+}
+
+interface Cloneable {
+    fun clone() {  }
+}
+
+open class Person(internal val name: String) {
+    open fun fullName() = println("Full name - $name")
+}
+
