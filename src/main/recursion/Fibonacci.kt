8fcb9c3a73da819c9cef0c8b9245edfac1974024
SSureshC <sureshchandak1@gmail.com>
2024-01-08 20:37:47 +0530

    Recursion

--- src/main/recursion/Fibonacci.kt
+++ src/main/recursion/Fibonacci.kt
@@ -1,5 +1,8 @@
 fun main() {
     println(fibonacci1(10))
+
+    print("0 1 ")
+    printFibonacci(0, 1, 5)
 }
 
 private fun fibonacci1(n: Int) : Int {
@@ -11,4 +14,16 @@ private fun fibonacci1(n: Int) : Int {
     val ans = fibonacci1(n - 1) + fibonacci1(n - 2)
 
     return ans
+}
+
+private fun printFibonacci(first: Int, second: Int, n: Int) {
+
+    if (n == 0) {
+        return
+    }
+
+    val next = first + second
+    print("$next ")
+
+    printFibonacci(second, next, n - 1)
 }
--- src/main/recursion/Power.kt
+++ src/main/recursion/Power.kt
@@ -1,12 +1,15 @@
 fun main(args: Array<String>) {
     println(power(2, 10))
     println(power(2, 5))
+    println(power1(2, 5))
     println(power(10, 2))
+    println(power(0, 2))
 }
 
 private fun power(n: Int, power: Int) : Int {
 
     // Base case
+    if (n == 0) return 0
     if (power == 0) return 1
 
     // Recursive relation
@@ -14,4 +17,18 @@ private fun power(n: Int, power: Int) : Int {
     val biggerProblem = n * smallerProblem
 
     return biggerProblem
+}
+
+private fun power1(n: Int, power: Int) : Int {
+
+    // Base case
+    if (n == 0) return 0
+    if (power == 0) return 1
+
+    return if (power % 2 == 0) {
+        power1(n, power / 2) * power1(n, power / 2)
+    } else {
+        power1(n, power / 2) * power1(n, power / 2) * n
+    }
+
 }
