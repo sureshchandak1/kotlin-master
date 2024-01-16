fb353dac6353822a86a2ed656320ffa2bff53757
SSureshC <sureshchandak1@gmail.com>
2024-01-16 12:01:14 +0530

    Pattern

--- src/main/kotlin/loops/pattern/Pattern11.kt
+++ src/main/kotlin/loops/pattern/Pattern11.kt
@@ -9,35 +9,43 @@ package loops.pattern
 * */
 
 fun main(args: Array<String>) {
+    println("-----------------------------------")
     printPattern1(5)
-    println("------------------")
+    println("-----------------------------------")
+    printPattern1(8)
+    println("-----------------------------------")
 }
 
 private fun printPattern1(n: Int) {
+    var starCount = 0
     var row = 1
     while (row <= n) {
 
         // print 1st triangle
         var col1 = 1
         while (col1 <= (n - row) + 1) {
-            print("$col1")
+            print("$col1 ")
             ++col1
         }
 
 
         // print 2nd triangle
-        if (row > 0) {
-            val starCount = 2
+        if (row > 1) {
+            starCount += 2
             var col2 = 0
             while (col2 < starCount) {
-                print("*")
+                print("* ")
                 ++col2
             }
         }
 
 
         // print 3rd triangle
-
+        var endPoint3 = (n - row) + 1
+        while (endPoint3 > 0) {
+            print("$endPoint3 ")
+            --endPoint3
+        }
 
         println()
 
