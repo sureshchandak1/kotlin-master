c225cce9421c0f16e99482231573344c9dc01628
SSureshC <sureshchandak1@gmail.com>
2024-02-17 20:42:28 +0530

    Recursion

---
+++ src/main/codingninjas/easy/RearrangeString.java
@@ -0,0 +1,54 @@
+package easy;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class RearrangeString {
+
+    public static void main(String[] args) {
+        System.out.println(rearrangeString("aac"));
+    }
+
+    public static String rearrangeString(String str) {
+
+        int size = str.length();
+        PriorityQueue<Character> pq = new PriorityQueue<>();
+
+        StringBuilder result = new StringBuilder();
+        int newStrIndex = 0;
+
+        int index = 0;
+        while (index < size) {
+            if (newStrIndex == 0) {
+                result.append(str.charAt(index));
+                newStrIndex++;
+            } else {
+                if (str.charAt(index) != result.charAt(newStrIndex - 1)) {
+                    result.append(str.charAt(index));
+                    newStrIndex++;
+                } else {
+                    Iterator<Character> itr = pq.iterator();
+                    while (itr.hasNext()) {
+                        char ch = pq.peek();
+                        if (ch != result.charAt(newStrIndex - 1)) {
+                            result.append(str.charAt(index));
+                            newStrIndex++;
+                            pq.poll();
+                        }
+                    }
+                    pq.offer(str.charAt(index));
+                }
+            }
+
+            index++;
+        }
+
+        if (newStrIndex + 1 == size) {
+            return result.toString();
+        }
+
+        return "NO SOLUTION";
+
+    }
+
+}
---
+++ src/main/dsa/_31/Counting.kt
@@ -0,0 +1,37 @@
+package _31
+
+fun main() {
+    printCountingHighToLow(5)
+    println("------------------------")
+    printCountingLowToHigh(5)
+}
+
+// Tail recursion
+private fun printCountingHighToLow(n: Int) {
+
+    // Base case
+    if (n == 0) {
+        return
+    }
+
+    // Processing
+    println(n)
+
+    // Recursion relation
+    printCountingHighToLow(n - 1)
+}
+
+// Head recursion
+private fun printCountingLowToHigh(n: Int) {
+
+    // Base case
+    if (n == 0) {
+        return
+    }
+
+    // Recursion relation
+    printCountingLowToHigh(n - 1)
+
+    // Processing
+    println(n)
+}
