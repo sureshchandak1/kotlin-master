95446f521380265e31bdde5a01e5d60d25ffc7e2
SSureshC <sureshchandak1@gmail.com>
2024-05-03 16:36:40 +0530

    Design a Stack that supports getMin() in O(1) time and O(1) extra space

--- src/main/dsa_450/stacks_and_queues/8. Min Stack.kt
+++ src/main/dsa_450/stacks_and_queues/8. Min Stack.kt
@@ -1,9 +1,10 @@
 package stacks_and_queues
 
-import java.util.Stack
+import java.util.*
 
 /**
  *   https://www.geeksforgeeks.org/problems/special-stack/1
+ *   https://leetcode.com/problems/min-stack/description/
  *   Time Complexity: O(1)
  *   Space Complexity: O(1)
  */
@@ -84,6 +85,47 @@ private class MinStack {
         return mini
     }
 }
+
+private class MinStack2 {
+
+    // stk1 keeps track of all the elements in the stack.
+    private val stack: Stack<Int> = Stack()
+
+    // minValuesStack keeps track of the minimum values at each state of the stack.
+    private val minValuesStack: Stack<Int> = Stack()
+
+    init {
+        // minValuesStack with the maximum value an integer can hold
+        minValuesStack.push(Int.MAX_VALUE)
+    }
+
+    // Method to push an element onto the stack. Updates the minimum value as well.
+    fun push(data: Int) {
+        // Push the value onto the stack.
+        stack.push(data)
+        // Push the smaller between the current value and the current minimum onto the minValuesStack.
+        minValuesStack.push(Math.min(data, minValuesStack.peek()))
+    }
+
+    // Method to remove the top element from the stack. Also updates the minimum value.
+    fun pop() {
+        // Remove the top element of the stack.
+        stack.pop()
+        // Remove the top element of the minValuesStack, which corresponds to the minimum at that state.
+        minValuesStack.pop()
+    }
+
+    // Method to retrieve the top element of the stack without removing it.
+    fun top(): Int {
+        // Peek the top element of the stack.
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return minValuesStack.peek()
+    }
+}
+
 
 
 
