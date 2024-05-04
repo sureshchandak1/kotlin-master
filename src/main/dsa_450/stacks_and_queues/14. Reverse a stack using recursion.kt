5d07984f4a7efcc0fa6e4d84fc9151be2d34212c
SSureshC <sureshchandak1@gmail.com>
2024-05-04 13:14:05 +0530

    Reverse a stack using recursion

---
+++ src/main/dsa_450/stacks_and_queues/14. Reverse a stack using recursion.kt
@@ -0,0 +1,50 @@
+package stacks_and_queues
+
+import java.util.Stack
+
+/**
+ *   https://www.geeksforgeeks.org/problems/reverse-a-stack/1
+ *   Time Complexity: O(N * N), where N is size of stack
+ *   Space Complexity: O(1)
+ */
+fun main() {
+    val stack = Stack<Int>()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+
+    println(stack.toString())
+    reverseStack(stack)
+    println(stack.toString())
+}
+
+private fun reverseStack(stack: Stack<Int>) {
+
+    if (stack.isEmpty()) {
+        return
+    }
+
+    val topElement = stack.pop()
+
+    reverseStack(stack)
+
+    pushBottom(stack, topElement)
+}
+
+private fun pushBottom(stack: Stack<Int>, data: Int) {
+
+    if (stack.isEmpty()) {
+        stack.push(data)
+        return
+    }
+
+    val topElement = stack.pop()
+
+    pushBottom(stack, data)
+
+    stack.push(topElement)
+}
+
+
+
