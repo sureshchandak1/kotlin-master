58742e69e06642a4af2f5dd485c3f1e88252b01f
SSureshC <sureshchandak1@gmail.com>
2024-05-13 20:00:44 +0530

    Top and Bottom view of binary tree

---
+++ src/main/dsa_450/binaryTrees/11. Top View of a tree.kt
@@ -0,0 +1,52 @@
+package binaryTrees
+
+import printArray
+import java.util.*
+
+fun main() {
+    val root = TreeBuilder.buildTree()
+
+    val ans = ArrayList<Int>()
+    topView(root, ans)
+    printArray(ans)
+}
+
+private fun topView(root: TreeNode?, ans: ArrayList<Int>) {
+    // level order traversal
+    if (root == null) {
+        return
+    }
+
+    // key = horizontal distance, value = node data
+    val map: MutableMap<Int, Int> = TreeMap()
+    // Pair: first = node, second = horizontalDistance
+    val q: Queue<Pair<TreeNode?, Int>> = LinkedList()
+    q.add(Pair(root, 0))
+
+    while (!q.isEmpty()) {
+        val temp = q.remove()
+
+        val node: TreeNode = temp.first!!
+        val hd = temp.second
+
+        if (!map.containsKey(hd)) {
+            map[hd] = node.data
+        }
+
+        // left
+        if (node.left != null) {
+            q.add(Pair(node.left, hd - 1))
+        }
+        // right
+        if (node.right != null) {
+            q.add(Pair(node.right, hd + 1))
+        }
+    }
+
+    for ((_, value) in map) {
+        ans.add(value)
+    }
+}
+
+
+
